8bc4897959d47ece8c98640e46c040a1f1c9a12b
Garrett Beatty <gman9732@gmail.com>
2017-01-17 18:36:40 -0500

    test for oauth

--- app/src/main/java/com/gbeatty/smsbackupandrestorepro/BackupService.java
+++ app/src/main/java/com/gbeatty/smsbackupandrestorepro/BackupService.java
@@ -1,70 +1,97 @@
 package com.gbeatty.smsbackupandrestorepro;
 
-import android.app.Service;
+import android.app.IntentService;
+import android.content.ContentResolver;
 import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Context;
 import android.content.Loader;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.IBinder;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 import com.gbeatty.smsbackupandrestorepro.models.Sms;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
 
-public class BackupService extends Service implements Loader.OnLoadCompleteListener<Cursor> {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public static final String BACKUP_RESULT = "com.gbeatty.smsbackupandrestorepro.BackupService.REQUEST_PROCESSED";
-    public static final String BACKUP_DATA = "com.gbeatty.smsbackupandrestorepro.BackupService.BACKUP_DATA";
+import static com.gbeatty.smsbackupandrestorepro.BaseActivity.PREF_ACCOUNT_NAME;
+import static com.gbeatty.smsbackupandrestorepro.BaseActivity.REQUEST_AUTHORIZATION;
+import static com.gbeatty.smsbackupandrestorepro.BaseActivity.SCOPES;
+import static com.google.android.gms.internal.zzs.TAG;
+
+public class BackupService extends IntentService  {
+
     private static final int LOADER_ID = 1;
     private Uri uri = Uri.parse("content://sms/");
     private String[] projection = {
             "_id", "address", "read", "body", "date", "type"
     };
-    private LocalBroadcastManager broadcaster;
-    private CursorLoader mCursorLoader;
+    private GoogleAccountCredential credential;
+    private com.google.api.services.gmail.Gmail mService = null;
+    private Exception mLastError = null;
+    private SharedPreferences settings;
+    private HttpTransport transport;
+    private JsonFactory jsonFactory;
+    private String account;
 
     public BackupService() {
-
+        super("BackupService");
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
+    public void onCreate(){
+        super.onCreate();
 
-        return null;
-    }
+        // Initialize credentials and service object.
+        credential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
 
-    @Override
-    public void onCreate() {
+        settings = PreferenceManager.getDefaultSharedPreferences(this);
+        account = settings.getString(PREF_ACCOUNT_NAME, null);
 
-        broadcaster = LocalBroadcastManager.getInstance(this);
-        mCursorLoader = new CursorLoader(this, uri, projection, null, null, null);
-        mCursorLoader.registerListener(LOADER_ID, this);
-        mCursorLoader.startLoading();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onDestroy() {
-
-        // Stop the cursor loader
-        if (mCursorLoader != null) {
-            mCursorLoader.unregisterListener(this);
-            mCursorLoader.cancelLoad();
-            mCursorLoader.stopLoading();
+        if(account != null){
+            credential.setSelectedAccountName(account);
         }
+
+        transport = AndroidHttp.newCompatibleTransport();
+        jsonFactory = JacksonFactory.getDefaultInstance();
+        mService = new com.google.api.services.gmail.Gmail.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("SMS Backup and Restore Pro")
+                .build();
+
     }
 
-    @Override
-    public void onLoadComplete(Loader<Cursor> loader, Cursor c) {
+    private void handleBackup() throws IOException {
+
+        ContentResolver resolver = getContentResolver();
+        Cursor c = resolver.query(uri, projection, null,null,null);
+
+        String user = "me";
+
+//        Label label = new Label().setName("garrett").setLabelListVisibility("labelShow").setMessageListVisibility("show");
+//        mService.users().labels().create(user, label).execute();
+
         if (c != null && c.getCount() > 0) {
-            Intent intent = new Intent(BACKUP_RESULT);
             c.moveToFirst();
             int totalSMS = c.getCount();
             int count = 0;
@@ -83,10 +110,22 @@ public class BackupService extends Service implements Loader.OnLoadCompleteListe
                     objSms.setFolderName("sent");
                 }
                 count++;
-                intent.putExtra(BACKUP_DATA, new int[]{count, totalSMS});
-                broadcaster.sendBroadcast(intent);
+                Log.d("count", "" + count);
                 c.moveToNext();
             }
         }
     }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        if(intent != null){
+            try {
+                handleBackup();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/gbeatty/smsbackupandrestorepro/MainActivity.java
+++ app/src/main/java/com/gbeatty/smsbackupandrestorepro/MainActivity.java
@@ -1,12 +1,34 @@
 package com.gbeatty.smsbackupandrestorepro;
 
+import android.accounts.AccountManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,24 +42,11 @@ public class MainActivity extends BaseActivity {
     TextView progressInfo;
     @BindView(R.id.progress)
     MaterialProgressBar progressBar;
-    private BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        receiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                int[] data = intent.getIntArrayExtra(BackupService.BACKUP_DATA);
-                int count = data[0];
-                int totalSMS = data[1];
-                double percent = (100 * count) / totalSMS;
-                progressBar.setProgress((int) percent);
-                progressInfo.setText(getResources().getString(R.string.progress_info, count, totalSMS));
-            }
-        };
         ButterKnife.bind(this);
     }
 
@@ -66,20 +75,94 @@ public class MainActivity extends BaseActivity {
         if (mCredential.getSelectedAccountName() == null) {
             getGoogleAccount(false);
         } else {
-            Intent serviceIntent = new Intent(this, BackupService.class);
-            startService(serviceIntent);
+            oAuthTest();
         }
+    }
+
+    private void oAuthTest(){
+        // Initialize credentials and service object.
+        GoogleAccountCredential credential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        String account = settings.getString(PREF_ACCOUNT_NAME, null);
+        credential.setSelectedAccountName(account);
+        new testOAuth(credential).execute();
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        LocalBroadcastManager.getInstance(this).registerReceiver((receiver), new IntentFilter(BackupService.BACKUP_RESULT));
     }
 
     @Override
     protected void onStop() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
         super.onStop();
+    }
+
+    /**
+     * An asynchronous task that handles the Gmail API call.
+     * Placing the API calls in their own task ensures the UI stays responsive.
+     */
+    private class testOAuth extends AsyncTask<Void, Void, Integer> {
+        private com.google.api.services.gmail.Gmail mService = null;
+        private Exception mLastError = null;
+
+        testOAuth(GoogleAccountCredential credential) {
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            mService = new com.google.api.services.gmail.Gmail.Builder(
+                    transport, jsonFactory, credential)
+                    .setApplicationName("Gmail API Android Quickstart")
+                    .build();
+        }
+
+        /**
+         * Background task to call Gmail API.
+         * @param params no parameters needed for this task.
+         */
+        @Override
+        protected Integer doInBackground(Void... params) {
+            try {
+                getDataFromApi();
+                return 1;
+            } catch (Exception e) {
+                mLastError = e;
+                cancel(true);
+                return 0;
+            }
+        }
+
+        /**
+         * @throws IOException
+         */
+        private void getDataFromApi() throws IOException {
+            // Get the labels in the user's account.
+            String user = "me";
+            mService.users().labels().get(user, "abc").execute();
+
+        }
+
+        @Override
+        protected void onCancelled() {
+
+            if (mLastError != null) {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            ((GooglePlayServicesAvailabilityIOException) mLastError)
+                                    .getConnectionStatusCode());
+                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                            ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                            MainActivity.REQUEST_AUTHORIZATION);
+                }
+                else{
+                    Intent serviceIntent = new Intent(getApplicationContext(), BackupService.class);
+                    startService(serviceIntent);
+                }
+            }
+        }
     }
 }
